8c5b443c1642fe94cf215750844e209dca2aebfe
Mike Grotton <mike.grotton@gmail.com>
2015-01-20 12:02:42 -0500

    db and table selector for leads

--- admin/parts/campaign-configuration.php
+++ admin/parts/campaign-configuration.php
@@ -23,18 +23,39 @@ Custom CSS Filename
 </select>
 </p>
 <p>
-<label for "inoutsel">Disposition</label>
-Inbound: <input type="checkbox" name="inbound" value="Yes">
-Outbound: <input type="checkbox" name="outbound" value="Yes">
+<label for="inoutsel">Disposition</label>
+Inbound: <input type="checkbox" name="inbound" id="inbound" value="Yes">
+Outbound: <input type="checkbox" name="outbound" id="outbound" value="Yes">
 </p>
 <p>
-<label for "customjs">Custom Javascript</label>
-<input type="text" name="customjs" placeholder="/path/to/js.js">
+<label for="customjs">Custom Javascript</label>
+<input type="text" name="customjs" id="customjs" placeholder="/path/to/js.js">
 </p>
 <p>
-<label for "customcss">Custom Javascript</label>
-<input type="text" name="customcss" placeholder="/path/to/css.css">
+<label for="customcss">Custom Javascript</label>
+<input type="text" name="customcss" id="customcss" placeholder="/path/to/css.css">
 </p>
+
+<p>
+<label for="lead-database">Lead Database</label>
+<select name="lead-database" id="lead-database">
+	<option value="scripts">Scripts</option>
+	<option value="mssql">Main SQL B</option>
+	<option value="cluster1">Cluster 1</option>
+	<option value="cluster2">Cluster 2</option>
+	<option value="cluster3">Cluster 3</option>
+</select>	
+</p>
+<p>
+<label for="lead-dbname">Lead DBName</label>
+<input type="text" name="lead-dbname" id="lead-dbname">
+</p>
+
+<p>
+<label for="lead-table">Lead Table</label>
+<input type="text" name="lead-table" id="lead-table">
+</p>
+
 </div>
 
 <button id="save_config">Save Configuration</button>
